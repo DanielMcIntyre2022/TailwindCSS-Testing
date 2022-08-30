360bf7ff29af040c73f4a632bad0a54dc636365c
Daniel <daniel-mcintyre@hotmail.com>
2022-08-30 13:47:51 -0400

    intial sidebar creation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-
 import './App.css';
+import { SideBar } from './SideBar';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='flex'>
+      <SideBar />
+    </div>
   )
 };
 
---
+++ src/SideBar.js
@@ -0,0 +1,14 @@
+
+
+
+export const SideBar = () => {
+    return (
+        <div className="fixed top-0 left-0 h-screen w-16 flex flex-col text-white shadow-lg bg-primary text-secondary">
+            <i>A</i>
+            <i>B</i>
+            <i>C</i>
+            <i>D</i>
+            <i>E</i>
+        </div>
+    )
+};
