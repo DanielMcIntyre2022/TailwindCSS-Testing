57706fc00a5e76f9e3be48c02856d01df2aca4c2
Daniel <daniel-mcintyre@hotmail.com>
2022-08-30 13:49:37 -0400

    sidebar config

--- src/SideBar.js
+++ src/SideBar.js
@@ -3,7 +3,7 @@
 
 export const SideBar = () => {
     return (
-        <div className="fixed top-0 left-0 h-screen w-16 flex flex-col text-white shadow-lg bg-primary text-secondary">
+        <div className="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col text-white bg-gray-900 text-white shadow-lg">
             <i>A</i>
             <i>B</i>
             <i>C</i>
